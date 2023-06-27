39e8e1f9bd7bd4309f4709dd9aa347b007ab5985
garrisonhh <garrisonhh@pm.me>
2023-06-27 11:07:02 -0400

    common utf8 + time functionality

--- lib/common/common.zig
+++ lib/common/common.zig
@@ -1,2 +1,3 @@
 pub usingnamespace @import("src/ref.zig");
 pub const time = @import("src/time.zig");
+pub const utf8 = @import("src/utf8.zig");
--- lib/common/src/time.zig
+++ lib/common/src/time.zig
@@ -2,5 +2,5 @@ const std = @import("std");
 
 /// get timestamp since epoch in seconds, with nanosecond precision
 pub fn now() f64 {
-    return @floatFromInt(f64, std.time.nanoTimestamp()) * 1e-9;    
+    return @floatFromInt(f64, std.time.nanoTimestamp()) * 1e-9;
 }
