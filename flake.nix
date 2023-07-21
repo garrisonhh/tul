56417915e782fe71a33800c59579a056d5407914
garrisonhh <garrisonhh@pm.me>
2023-07-21 14:18:59 -0400

    set up and run nix fmt

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = github:NixOs/nixpkgs/nixos-23.05;
-    zig.url     = github:mitchellh/zig-overlay;
+    zig.url = github:mitchellh/zig-overlay;
   };
 
   outputs = { self, zig, nixpkgs }:
@@ -25,32 +25,32 @@
             with pkgs.lib.strings;
             concatStrings (intersperse " " buildArgs);
         in
-          mkDerivation {
-            name = name;
-            src = self;
+        mkDerivation {
+          name = name;
+          src = self;
 
-            nativeBuildInputs = inputs;
-            buildPhase = ''
-              export HOME=$NIX_BUILD_TOP
-              zig build ${args}
-            '';
+          nativeBuildInputs = inputs;
+          buildPhase = ''
+            export HOME=$NIX_BUILD_TOP
+            zig build ${args}
+          '';
 
-            installPhase = ''
-              mkdir -p $out/bin/
-              mkdir -p $bin/
-              install zig-out/bin/${name} $bin/
-              install zig-out/bin/${name} $out/bin/
-            '';
+          installPhase = ''
+            mkdir -p $out/bin/
+            mkdir -p $bin/
+            install zig-out/bin/${name} $bin/
+            install zig-out/bin/${name} $out/bin/
+          '';
 
-            # TODO learn more about outputs and what is expected. I think using
-            # bin by default makes sense for this project, but I would like to
-            # understand more
-            outputs = [ "bin" "out" ];
-          };
+          # TODO learn more about outputs and what is expected. I think using
+          # bin by default makes sense for this project, but I would like to
+          # understand more
+          outputs = [ "bin" "out" ];
+        };
 
       packages = {
-        default = makePackage [];
-        release = makePackage ["-Doptimize=ReleaseFast"];
+        default = makePackage [ ];
+        release = makePackage [ "-Doptimize=ReleaseFast" ];
       };
 
       # app config for 'nix run'
@@ -64,9 +64,10 @@
         tul = tulApp;
       };
     in
-      {
-        apps.${system} = apps;
-        devShells.${system}.default = packages.default;
-        packages.${system} = packages;
-      };
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+      apps.${system} = apps;
+      devShells.${system}.default = packages.default;
+      packages.${system} = packages;
+    };
 }
